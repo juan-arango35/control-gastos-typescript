bd8bd712a1b5d555b1fcac507eadbb7efd91add7
juan arango <juanarangozapata35@gmail.com>
2025-01-10 12:13:03 -0500

    agrega funcion de modeda dolar

---
+++ src/components/AmountDisplay.tsx
@@ -0,0 +1,22 @@
+type AmountDisplayProps = {
+    label: string;
+    amount: number;
+}
+
+function formatCurrency(amount: number) {
+  return new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+  }).format(amount);
+}
+
+
+const AmountDisplay = ({label, amount}:AmountDisplayProps) => {
+  return (
+    <p className="text-2xl font-bold  text-blue-600 text-center" >
+        <span className="font-black">{label} :</span> <span className="text-black">{formatCurrency(amount)}</span>
+    </p>
+  )
+}
+
+export default AmountDisplay
