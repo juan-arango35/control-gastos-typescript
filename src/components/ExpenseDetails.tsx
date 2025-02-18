7542d7c9c3e9bde3be1868534a620004eb9c541c
juan arango <juanarangozapata35@gmail.com>
2025-02-18 07:26:32 -0500

    agrega iconos de lista de gastos

--- src/components/ExpenseDetails.tsx
+++ src/components/ExpenseDetails.tsx
@@ -1,5 +1,7 @@
+import { useMemo } from "react";
 import { Expense } from "../types/index";
 import AmountDisplay from "./AmountDisplay";
+import { categories } from "../data/categories";
 type ExpenseDetailsProps = {
   expense: Expense;
 };
@@ -16,10 +18,16 @@ export function formatDate(dateStr: string): string {
 }
 
 const ExpenseDetails = ({ expense }: ExpenseDetailsProps) => {
+
+  const cateforyInfo = useMemo(()=>categories.filter(cat => cat.id === expense.category)[0],[expense])
+  console.log(cateforyInfo)
   return (
-    <div className="bg-white shadow-lg  py-10 rounded-lg border-b border-gray-200 flex gap-5 items-center">
-      <div></div>
+    <div className="bg-white shadow-lg   py-10 rounded-lg border-b border-gray-200 flex gap-5 items-center pr-7">
       <div>
+        <img src={`/icono_${cateforyInfo.icon}.svg`} alt="Icono gasto" className="w-20"/>
+      </div>
+      <div className="flex-1 space-y-3 text-sm font-bold uppercase text-slate-500">
+        <p>{cateforyInfo.name}</p>
         <p className="text-slate-600 text-sm" key={expense.id}>
           {expense.expenseName}
         </p>
