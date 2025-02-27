865f5dd9ed778f6be8125d6b2a68a9aea30cdaf4
juan arango <juanarangozapata35@gmail.com>
2025-02-27 09:14:47 -0500

    agrega animacion circular en porcentaje

--- src/components/BudgetTracker.tsx
+++ src/components/BudgetTracker.tsx
@@ -1,12 +1,27 @@
+import { CircularProgressbar, buildStyles } from "react-circular-progressbar";
+import "react-circular-progressbar/dist/styles.css";
 import { useBudget } from "../hooks/useBudget";
 import AmountDisplay from "./AmountDisplay";
 const BudgetTracker = () => {
   const { state, totalExpenses, remainingBudget } = useBudget();
+  const percentage = +((totalExpenses / state.budget) * 100).toFixed(2);
+  console.log(percentage);
 
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 gap-5">
       <div className="flex justify-center">
-        <img src="/grafico.jpg" alt="Grafica de gastos" />
+        {/*  <img src="/grafico.jpg" alt="Grafica de gastos" /> */}
+        <CircularProgressbar
+          value={percentage}
+          styles={buildStyles({
+            pathColor: percentage ===100 ? "#dc2626" : "#3b82f6",
+            textSize: "10px",
+            textColor: percentage ===100 ? "#dc2626" : "#3b82f6",
+
+            trailColor: "#f5f5f5",
+          })}
+          text={`${percentage}% Gastado`}
+        />
       </div>
       <div className="flex flex-col justify-center gap-8">
         <button
